abbb38ecd417e42c60a00d918aab7e73438a0d60
Ford Henley <info@fordswebhelp.com>
2025-08-19 16:53:32 -0500

    Completed C# story program exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,9 +2,31 @@
 {
     public class Program
     {
+        public static string  SocialEngineering()
+        {
+            //Exercise 1 - Social Engineering
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your age?");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is your mother's maiden name?");
+            string motherName = Console.ReadLine();
+            Console.WriteLine("What is the name of your first pet?");
+            string petsFirstName = Console.ReadLine();
+            Console.WriteLine("Name of the city where you were born?");
+            string birthCity = Console.ReadLine();
+            
+            //output
+           string result = $"Your name is {name} and your age is {age}, your mother's maiden name is {motherName}. Your first pet's name is {petsFirstName} and you were born in {birthCity}. Now I have your identity!! Haha!!";
+           
+           Console.WriteLine(result);
+           return result;
+            
+            
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SocialEngineering();
         }
     }
 }
